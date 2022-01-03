81cdc9dd9c7553dbdb14df723cc14936a287b356
sonaza <sonaza@iki.fi>
2022-01-03 09:14:49 +0200

    Deploy script and update logging

--- card_rotation_update.sh
+++ card_rotation_update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /var/www/sonaza.com/sifas-cards.subdomain
-python3 CardRotations.py $@
+
+now=$(date)
+echo "----------------------------------" >> update.log
+echo "Update log for $now" >> update.log
+unbuffer -p python3 CardRotations.py $@ | tee -a update.log
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git pull
+chown -R sonaza.com:sonaza.com .
+./card_rotation_update.sh
