311f4efcda7e1587baa29dc589adb5606f6ad65c
sonaza <sonaza@iki.fi>
2022-01-04 14:44:41 +0200

    Fix update script

--- card_rotation_update.sh
+++ card_rotation_update.sh
@@ -2,6 +2,6 @@
 cd /var/www/sonaza.com/sifas-cards.subdomain
 
 now=$(date)
-echo "----------------------------------" >> update.log
-echo "Update log for $now" >> update.log
-unbuffer -p python3 CardRotations.py $@ | tee -a update.log
+echo "----------------------------------"
+echo "Update log for $now"
+python3 CardRotations.py $@
