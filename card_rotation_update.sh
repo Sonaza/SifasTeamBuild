c6cae5561beda8771444da2aeb3dee07fc7cd8e5
Sonaza <sonaza@iki.fi>
2022-01-27 00:25:04 +0200

    Run via pipenv

--- card_rotation_update.sh
+++ card_rotation_update.sh
@@ -5,4 +5,4 @@ cd /var/www/sonaza.com/sifas-cards.subdomain
 now=$(date)
 echo "----------------------------------"
 echo "Update log for $now"
-python3 CardRotations.py $@
+pipenv run python3 CardRotations.py $@
