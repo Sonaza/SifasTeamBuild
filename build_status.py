f2f6cec80d384d0fce9974e2b8cec9348dd2c826
sonaza <sonaza@iki.fi>
2022-01-29 23:43:27 +0200

    Fixed build status script

--- build_status.py
+++ build_status.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3.7
 
 try:
-	buildstatus = open("build.status", "r")
+	buildstatus = open("/var/www/sonaza.com/sifas-cards.subdomain/build.status", "r")
 except FileNotFoundError:
 	print("Build status file missing or unreadable.")
-	exit(1)
+	exit(-1337)
 	
-status = buildstatus.read().split("\n", 1)
+status = [x.strip() for x in buildstatus.read().split("\n", 2)]
 
 if status[1] == "success":
+	print("BUILD SUCCESSFUL!")
 	exit(0)
 
-else:
+elif status[1] == "failed":
 	print("BUILD FAILED!")
 	print(status[2])
 	exit(1)
 
-
+exit(-1337)
