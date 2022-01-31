5cf5f283c894b1524c0949604298c36f36b4e4c6
sonaza <sonaza@iki.fi>
2022-01-31 02:08:10 +0200

    Fixed build status again because stupid

--- build_status.py
+++ build_status.py
@@ -18,11 +18,10 @@ except json.decoder.JSONDecodeError:
 	print("Build status file corrupted/not valid json.")
 	exit(-1337)
 	
-today = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
 timestamp = datetime.fromisoformat(status['timestamp'])
+today = datetime.now(timezone.utc) - timedelta(hours=6, minutes=6)
 
-delta = timedelta(hours=6, minutes=6)
-has_built_today = (today.day == (timestamp - delta).day)
+has_built_today = (today.day == timestamp.day)
 
 already_handled = False
 if status['handled'] != None:
