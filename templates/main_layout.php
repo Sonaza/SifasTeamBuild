d3e83debffec584a1ce879101894d4cfe21fa443
Sonaza <sonaza@iki.fi>
2022-02-16 15:45:55 +0200

    Fix preload font stuff

--- templates/main_layout.php
+++ templates/main_layout.php
@@ -8,10 +8,10 @@
 {% for item in preloads %}
 	<link rel="preload" href="{{ item['path'] }}" as="{{ item['type'] }}">{% endfor %}
 	<link rel="preload" href="/js/vendor/angular/angular-combined.min.js" as="script">
-	<link rel="preload" href="/css/fonts/Roboto-Regular-Latin.woff2" as="font">
-	<link rel="preload" href="/css/fonts/Roboto-Bold-Latin.woff2" as="font">
-	<link rel="preload" href="/css/fonts/Montserrat-Regular-latin.woff2" as="font">
-	<link rel="preload" href="/css/fonts/Montserrat-Bold-latin.woff2" as="font">
+	<link rel="preload" href="/css/fonts/Roboto-Regular-Latin.woff2" as="font" crossorigin="anonymous">
+	<link rel="preload" href="/css/fonts/Roboto-Bold-Latin.woff2" as="font" crossorigin="anonymous">
+	<link rel="preload" href="/css/fonts/Montserrat-Regular-latin.woff2" as="font" crossorigin="anonymous">
+	<link rel="preload" href="/css/fonts/Montserrat-Bold-latin.woff2" as="font" crossorigin="anonymous">
  	<link rel="stylesheet" href="/css/vendor/font-awesome/font-awesome.min.css">
 	<link rel="stylesheet" href="{{ cache_buster('/css/public.min.css') }}">
 	<meta property="og:type" content="website">
