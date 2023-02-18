9af92b0fb46341f3b227879445d53c783dbe6093
Sonaza <sonaza@iki.fi>
2023-02-18 19:16:40 +0200

    Added yay emotes

--- tools/icons/scaler.py
+++ tools/icons/scaler.py
@@ -2,38 +2,45 @@ import os
 from PIL import Image
 from glob import glob
 
+def mkdir(path):
+	try:
+		os.mkdir(path)
+	except:
+		pass
+
 border = ""
 # border = "-border"
 
-icons = glob(f"large{border}/*.png")
-# icons = ['large/icon_107.png']
+categories = {
+	# 'general' : [16, 32, 48, 64, 128],
+	'yay'     : [64],
+}
 
-for size in [16, 32, 48, 64, 128]:
-# for size in [16]:
-	sizedir = f'{size}'
-	if not os.path.exists(sizedir):
-		try:
-			os.mkdir(sizedir)
-		except:
-			pass
-	
-	for icon in icons:
-		name, ext = os.path.splitext(os.path.basename(icon))
-		name = name.split('-')
-		name = f"{name[0]}-{name[1]}"
-		target_path = f'{size}/{name}{border}{ext}'
+for directory, category_sizes in categories.items():
+	icons = glob(os.path.join(directory, f"large{border}/*.png"))
+
+	for size in category_sizes:
+		sizedir = os.path.join(directory, str(size))
 		
-		if os.path.exists(target_path):
-			continue
+		mkdir(sizedir)
+		
+		for icon in icons:
+			name, ext = os.path.splitext(os.path.basename(icon))
+			name = name.split('-')
+			name = f"{name[0]}-{name[1]}"
+			target_path = f'{sizedir}/{name}{border}{ext}'.replace('\\', '/')
 			
-		im = Image.open(icon)
-		im = im.convert('RGBA')
-		# print(im.format, im.size, im.mode)
+			if os.path.exists(target_path):
+				continue
+				
+			im = Image.open(icon)
+			im = im.convert('RGBA')
+			# print(im.format, im.size, im.mode)
+			
+			im.thumbnail((size, size), Image.BICUBIC)
+			
+			# print(f".grid-icon .{os.path.basename(icon).split('.')[0]} {{ background-image: url('icons/32/{os.path.basename(icon)}'); }}")
+			
+			print("Saving", target_path)
+			im.save(target_path)
 		
-		im.thumbnail((size, size), Image.BICUBIC)
-		
-		# print(f".grid-icon .{os.path.basename(icon).split('.')[0]} {{ background-image: url('icons/32/{os.path.basename(icon)}'); }}")
-		
-		print("Saving", target_path)
-		im.save(target_path)
-	
